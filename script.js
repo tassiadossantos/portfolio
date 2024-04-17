b61c7e239f000cbee59d59451c5e4a1001c4dbdf
Tassia dos Santos Silva <161767119+tassiadossantos@users.noreply.github.com>
2024-04-17 10:06:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['(quase) <span>Front-End Developer</span>', '<span>Content Marketing</span>', '<span>Growth Hacker</span>'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
