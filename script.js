225d27ad10896e676f5c6f5f9cc47c6c2a58634b
Tassia dos Santos Silva <161767119+tassiadossantos@users.noreply.github.com>
2024-04-18 12:30:55 -0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const typed = new Typed('.multiple-text', {
-    strings: ['(quase) <span>Front-End Developer</span>', '<span>Content Marketing</span>', '<span>Growth Hacker</span>'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "(quase) <span>Front-End Developer</span>",
+    "<span>Content Marketing</span>",
+    "<span>Growth Hacker</span>",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
+
+// Função para alternar a visibilidade da barra de navegação
+function toggleNavbar() {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("active");
+}
+
+// Adicionando evento de clique ao ícone do menu
+document.getElementById("menu-icon").addEventListener("click", toggleNavbar);
