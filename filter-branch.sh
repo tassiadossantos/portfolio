cfaa344927a1ed8150794c4f1f866ad897f2f923
Tassia dos Santos <tassiadossantos@github.com>
2026-07-19 18:30:27 -0300

    fix: favicon path relativo

---
+++ filter-branch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$GIT_AUTHOR_NAME" = "Replit Agent" ]; then
+  export GIT_AUTHOR_NAME="Tassia dos Santos"
+fi
+if [ "$GIT_COMMITTER_NAME" = "Replit Agent" ]; then
+  export GIT_COMMITTER_NAME="Tassia dos Santos"
+fi
